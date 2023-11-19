af24f3c9157ff6c9bcd90ceb0dab2f34e415754e
apikmeister <afiq.mohamad90@gmail.com>
2023-11-19 10:23:05 +0800

    Refactor transaction list widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,50 +11,70 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 500,
-        child: ListView.builder(
-            itemCount: transactions.length,
-            itemBuilder: (ctx, index) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black, width: 2)),
-                      padding: EdgeInsets.all(10),
-                      alignment: Alignment.center,
-                      child: Text(
-                        '\RM${transactions[index].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.red,
-                        ),
-                      ),
+        child: transactions.isEmpty
+            ? Column(
+                children: <Widget>[
+                  Text(
+                    'No transaction added yet!',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  )
+                ],
+              )
+            : ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Row(
                       children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black, width: 2)),
+                          padding: EdgeInsets.all(10),
+                          alignment: Alignment.center,
+                          child: Text(
+                            '\RM${transactions[index].amount.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.red,
+                            ),
                           ),
                         ),
-                        // Text(e.date.toString())
-                        Text(
-                          DateFormat('dd/MM/yyy')
-                              .format(transactions[index].date),
-                          style: TextStyle(color: Colors.grey),
-                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              transactions[index].title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            // Text(e.date.toString())
+                            Text(
+                              DateFormat('dd/MM/yyy')
+                                  .format(transactions[index].date),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
-              );
-            })
+                    ),
+                  );
+                })
         // .toList()),
         );
   }
